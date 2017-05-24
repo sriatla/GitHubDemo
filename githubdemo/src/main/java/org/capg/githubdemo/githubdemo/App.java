b81b568d4ca08dac200d980c7d369d3beb289e50
lnc <lnc@DIN76000014.corp.capgemini.com>
2017-05-24 18:19:09 +0530

    first commit

---
+++ githubdemo/src/main/java/org/capg/githubdemo/githubdemo/App.java
@@ -0,0 +1,13 @@
+package org.capg.githubdemo.githubdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
